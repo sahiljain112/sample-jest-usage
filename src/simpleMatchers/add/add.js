ed6611d2bbe14f12e263bdc0d1b8d63cc7ad512c
Sahil Jain <sahiljain112@gmail.com>
2017-06-06 15:24:34 +0530

    Initial setup

---
+++ src/simpleMatchers/add/add.js
@@ -0,0 +1,6 @@
+/*
+* Created by Sahil on 06/07/17
+* */
+
+const add = (a,b) => a + b;
+export default add;
---
+++ src/simpleMatchers/add/add.test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sahil on 6/6/17.
+ */
+
+import add from './add';
+
+test('Add 3 and 2 gives 5', () => {
+    expect(add(3,2)).toBe(5);
+})
